7cbc07dff1d3be96220bbbdb17327aa2729e527b
farukh sajjad <farukhsajjad110@gmail.com>
2025-10-12 16:38:43 +0500

    fixed issue of profile icon

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -177,59 +177,20 @@ const Header: React.FC = () => {
             >
               <ul className="py-2 text-sm text-gray-700">
                 <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.118a7.5 7.5 0 0115 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.5-1.632z"
-                    />
-                  </svg>
-                  <Link href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  
+                  <Link href="#" className="block px-4 py-2 w-full hover:bg-gray-100">
                     Log In
                   </Link>
                 </li>
                 <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.118a7.5 7.5 0 0115 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.5-1.632z"
-                    />
-                  </svg>
-                  <Link href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  
+                  <Link href="#" className="block px-4 py-2 w-full hover:bg-gray-100">
                     Sign Up
                   </Link>
                 </li>
                 <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.118a7.5 7.5 0 0115 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.5-1.632z"
-                    />
-                  </svg>
-                  <Link href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  
+                  <Link href="#" className="block px-4 py-2 w-full hover:bg-gray-100">
                     My Account
                   </Link>
                 </li>
